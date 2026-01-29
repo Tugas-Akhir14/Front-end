9b999413384af68c5405c152367d1bc9bc8b2656
CallMeMarcel <pedromhutagaol@gmail.com>
2026-01-29 22:41:27 +0700

    ampunn

--- next.config.js
+++ next.config.js
@@ -2,21 +2,26 @@
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || ''; // fallback ke string kosong
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Hanya buat rewrites jika API_URL ada
   async rewrites() {
-    // jika API_URL tidak ada, jangan buat rewrite
     if (!API_URL) return [];
     return [
       {
-        source: '/public/:path*',
-        destination: `${API_URL}/public/:path*`,
+        source: '/public/:path*',          // route yang diakses di frontend
+        destination: `${API_URL}/public/:path*`, // route di backend
       },
     ];
   },
+  
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // supaya build tidak gagal karena lint error
   },
-  images: { unoptimized: true },
+
+  images: {
+    unoptimized: true, // matikan optimisasi Next.js, karena image diambil dari API
+  },
 };
 
 module.exports = nextConfig;
